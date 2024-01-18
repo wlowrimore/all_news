f2da58fecd1b9abd672443d6c39686e3d323a08e
William Lowrimore <wlowrimore@gmail.com>
2024-01-17 22:50:23 -0600

    begins consolidating navlinks into grouped dropdown items and adds BusinessNews

---
+++ src/app/business-news/page.jsx
@@ -0,0 +1,11 @@
+import BusinessNews from "../components/businessNews/BusinessNews"
+
+const page = () => {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-between p-24 mx-[16rem]'>
+      <BusinessNews />
+    </div>
+  )
+}
+
+export default page
--- src/app/components/header/Navbar.jsx
+++ src/app/components/header/Navbar.jsx
@@ -1,17 +1,33 @@
 'use client'
 
+import { useState } from "react"
 import Link from "next/link"
 const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const handleClick = () => {
+    setIsVisible(!isVisible)
+  }
+
   return (
     <nav className='flex justify-between items-center py-2 px-24 text-lg text-white bg-gray-800'>
       <Link href='/' className='py-2 px-3 transition-all text-xl tracking-wider text-red-400 hover:bg-gray-700 duration-300'>NLN</Link>
       <Link href='/world-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'>World News</Link>
       <Link href='/us-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'>US News</Link>
       <Link href='/sports-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'>Sports</Link>
-      <Link href='/movie-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'>Movies</Link>
-      <Link href='/theater-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'>Theater</Link>
-      <Link href='/arts-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'>Arts</Link>
-      <Link href='/books-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'>Literature</Link>
+
+      <div className='py-2 px-3 transition-all hover:bg-gray-700 duration-300 cursor-pointer' onClick={handleClick}>Arts & Entertainment</div>
+      {isVisible && (
+        <div className='absolute z-10 w-[10.6%] left-[45%] ml-[0.20rem] top-[6%] flex flex-col bg-gray-800'>
+          <Link href='/movie-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'><p onClick={handleClick}>Movies</p></Link>
+          <Link href='/theater-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'><p onClick={handleClick}>Theater</p></Link>
+          <Link href='/arts-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'><p onClick={handleClick}>Arts</p></Link>
+          <Link href='/literary-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'><p onClick={handleClick}>Literature</p></Link>
+        </div>
+      )}
+
+
+      <Link href='/business-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'>Business</Link>
       <Link href='/tech-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'>Technology</Link>
       <Link href='/science-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'>Science</Link>
       <Link href='/food-news' className='py-2 px-3 transition-all hover:bg-gray-700 duration-300'>Food</Link>
