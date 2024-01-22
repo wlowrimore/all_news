e3cce37e1e51fabc7c2f24d2983ad9e21c4e9658
William Lowrimore <wlowrimore@gmail.com>
2024-01-21 22:54:09 -0600

    ccrrects margin-x on small device for loading spinner

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className='relative flex min-h-screen flex-col items-center justify-center p-24 mx-[16rem]'>
+    <div className='relative flex min-h-screen flex-col items-center justify-center p-24 md:mx-[16rem]'>
       <div className='loader'></div>
       <div className='absolute animate-pulse transition duration-500'>
         <h1 className='text-center text-6xl font-bold tracking-wider text-red-400'>NLN</h1>
