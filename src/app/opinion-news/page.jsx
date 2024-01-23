6e6b296ebb18b5d01008f8cf32e2b9e34990b825
William Lowrimore <wlowrimore@gmail.com>
2024-01-23 12:40:15 -0600

    adds metadata

--- src/app/opinion-news/page.jsx
+++ src/app/opinion-news/page.jsx
@@ -1,6 +1,17 @@
 import OpinionNews from "../components/opinionNews/OpinionNews"
 import { wait } from "../utils/wait"
 
+export const metadata = {
+  title: 'News Link Now | Opinion News',
+  description: 'Get up-to-date news from the New York Times. Search for anything from a specific person to a specific topic.  Be sure to use the filter field to narrow down your search for a more focused result.',
+  keywords: 'New York Times, News, Search, Filter, News Link Now, Insider News, Food News, Health News, Tech News, Sports News, Opinion News, World News, Food, Health, Tech, Sports, Opinion, World, Insider, Search, Filter, News Link Now, 2023',
+  author: 'William Lowrimore, News Link Now, 2023, https://news-link-now.vercel.app, https://github.com/wlowrimore, fakenamedev, @wlowrimore, wlowrimore',
+  robots: 'index, follow',
+  viewport: 'width=device-width, initial-scale=1.0',
+  charset: 'utf-8',
+  themeColor: '#fff',
+}
+
 const page = async () => {
   await wait(2500)
   return (
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
 import HomePageStories from "./components/HomePageStories";
 import { Noticia_Text } from "next/font/google";
 import { wait } from "./utils/wait";
-import MainSearch from "./components/searchModal/MainSearch";
 
 const noticia = Noticia_Text({ subsets: ['latin'], weight: ['400', '700'] })
+
+export const metadata = {
+  title: 'News Link Now | Home',
+  description: 'Get up-to-date news from the New York Times. Search for anything from a specific person to a specific topic.  Be sure to use the filter field to narrow down your search for a more focused result.',
+  keywords: 'New York Times, News, Search, Filter, News Link Now, Insider News, Food News, Health News, Tech News, Sports News, Opinion News, World News, Food, Health, Tech, Sports, Opinion, World, Insider, Search, Filter, News Link Now, 2023',
+  author: 'William Lowrimore, News Link Now, 2023, https://news-link-now.vercel.app, https://github.com/wlowrimore, fakenamedev, @wlowrimore, wlowrimore',
+  robots: 'index, follow',
+  viewport: 'width=device-width, initial-scale=1.0',
+  charset: 'utf-8',
+  themeColor: '#fff',
+}
+
 export default async function Home() {
   await wait(2500)
   return (
